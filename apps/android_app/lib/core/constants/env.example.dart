6db5b744aa62d31432e177c515545c50c33633cd
cesar <cesar@MacBook-Pro-de-Cesar.local>
2026-04-07 16:40:15 -0600

    feat: Cambios en env.example.dart

--- apps/android_app/lib/core/constants/env.example.dart
+++ apps/android_app/lib/core/constants/env.example.dart
@@ -1,8 +1,8 @@
 // lib/core/constants/env.dart
 // NUNCA subas este archivo a git
 
-const String mapboxPublicToken = 'pk.eyJ1IjoiaXZhbjE0MyIsImEiOiJjbW5kcHB0djkxam54Mm9vbGpib3JwZHo5In0.J3fwimBrfjGgEpgy1LXJVw';
+const String mapboxPublicToken = 'pk.djo';
 
-const String supabaseUrlEnv = 'https://qewqnirwuptcudoflgkd.supabase.co';
+const String supabaseUrlEnv = 'http';
 
-const String supabaseAnonKeyEnv = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFld3FuaXJ3dXB0Y3Vkb2ZsZ2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4Mzg1NjQsImV4cCI6MjA5MDQxNDU2NH0.5rUOin2SZlq-fRpWqr4Vzd97iI4JDca3-Zl7N3qy6lc';
+const String supabaseAnonKeyEnv = 'dfs';
